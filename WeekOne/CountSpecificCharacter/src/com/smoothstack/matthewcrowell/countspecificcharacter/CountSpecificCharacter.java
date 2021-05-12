fcb51761a79c8e5deb2f9b01f5562a227f908641
Matthew-Crowell <Matthew.Crowell@Smoothstack.com>
2021-05-12 12:17:11 -0500

    Updated CountSpecificCharacter.

--- WeekOne/CountSpecificCharacter/src/com/smoothstack/matthewcrowell/countspecificcharacter/CountSpecificCharacter.java
+++ WeekOne/CountSpecificCharacter/src/com/smoothstack/matthewcrowell/countspecificcharacter/CountSpecificCharacter.java
@@ -17,10 +17,11 @@ public class CountSpecificCharacter {
 	private Character triggerCharacter;
 
 	/**
-	 * Default constructor, sets trigger character to 'a'.
+	 * Default constructor, sets member variables to null.
 	 */
 	CountSpecificCharacter() {
-		this('a');
+		this.triggerCharacter = null;
+		this.characterCount = null;
 	}
 
 	/**
@@ -34,41 +35,91 @@ public class CountSpecificCharacter {
 	}
 
 	/**
-	 * Instantiates CountSpecificCharacter object, calls countOccurences
-	 * on object, and handles FileNotFoundExceptions.
+	 * Handles command line arguments and displays number of times character occurs.
 	 *
 	 * @param args String[] where first argument is the file to analyze
 	 */
 	public static void main(String[] args) {
-		CountSpecificCharacter count = new CountSpecificCharacter('e');
-		try {
-			System.out.println(count.countOccurrences(args));
-		} catch (FileNotFoundException e) {
-			System.out.println("Unable to locate file, " + args[0] + ", please try again.");
-		}
+		CountSpecificCharacter app = null;
+		String filename = null;
 
+		switch (args.length > 0 ? args[0] : "--help") {
+			case ("--help"):
+			case ("-h"):
+				app = new CountSpecificCharacter();
+				app.printHelp();
+				break;
+			case ("--character"):
+			case ("-c"):
+				app = new CountSpecificCharacter(args[1].toCharArray()[0]);
+				if (args.length > 2) {
+					filename = new String(args[3]);
+				} else {
+					System.out.print("File to be searched: ");
+					Scanner scanner = new Scanner(System.in);
+					filename = new String(scanner.next());
+				}
+				break;
+			case ("--file"):
+			case ("-f"):
+				filename = new String(args[1]);
+				if (args.length > 2) {
+					app = new CountSpecificCharacter(args[3].charAt(0));
+				} else {
+					Scanner scanner = new Scanner(System.in);
+					System.out.print("Character to seek: ");
+					Character c = scanner.next().charAt(0);
+					app = new CountSpecificCharacter(c);
+				}
+				break;
+			default:
+				app = new CountSpecificCharacter();
+				app.printHelp();
+				break;
+		}
+		if (app != null && filename != null) {
+			System.out.println(app.countOccurrences(filename));
+		}
 	}
 
 	/**
-	 * Scans a file, args[0], for a specified character.
+	 * Scans a file to count occurrences of a specified character.
 	 *
-	 * @param args String[] where first argument is file to be scanned
-	 * @return Integer representing number of occurences of character in file
-	 * @throws FileNotFoundException if unable to locate specified file
+	 * @param filename String filename of file to be searched
+	 * @return Integer representing number of occurences of character in file or -1 if no file was searched
 	 */
-	public Integer countOccurrences(String[] args) throws FileNotFoundException {
-		StringBuilder line = new StringBuilder();
-		Scanner scanner = new Scanner(new BufferedReader(new FileReader(args[0])));
-		while (scanner.hasNext()) {
-			line.append(scanner.next());
-			char[] letters = line.toString().toUpperCase(Locale.ROOT).toCharArray();
-			for (char letter : letters) {
-				if (letter == triggerCharacter) {
-					characterCount++;
+	private Integer countOccurrences(String filename) {
+		try {
+			Scanner scanner = new Scanner(new BufferedReader(new FileReader(filename)));
+			while (scanner.hasNext()) {
+				for (char letter : scanner.next().toUpperCase(Locale.ROOT).toCharArray()) {
+					if (letter == triggerCharacter) {
+						characterCount++;
+					}
 				}
 			}
-			line.setLength(0);
+		} catch (FileNotFoundException e) {
+			System.out.print("File not found.  Enter 'q' to exit.\nFile to be searched: ");
+			Scanner scanner = new Scanner(System.in);
+			filename = scanner.next();
+			if(filename.equals("q")){
+				characterCount = -1;
+			}
+			else{
+				countOccurrences(filename);
+			}
 		}
 		return characterCount;
+	}
+
+	/**
+	 * Print command line help to terminal on standard output.
+	 */
+	private void printHelp() {
+		System.out.println("Class to count the number of times a character appears in a given file.\n" +
+				"Usage:\n\tjava com.smoothstack.matthewcrowell.countspecificcharacter.CountSpecificCharacter " +
+				"<--file | -f> <path/to/file> <--character | -c> <character>\n\tjava com.smoothstack.matthew" +
+				"crowell.countspecificcharacter.CountSpecificCharacter <--character | -c> <character> " +
+				"<--file | -f> <path/to/file>");
 	}
 }
