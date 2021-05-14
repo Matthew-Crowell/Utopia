e74a2665f4a265736a1355b89e06b78e2e00190b
Matthew-Crowell <Matthew.Crowell@Smoothstack.com>
2021-05-14 09:20:24 -0500

    Unit tests for LineTest implemented.

--- WeekOne/DayFour/LineTest/test/com/smoothstack/matthewcrowell/linetest/LineTestClass.java
+++ WeekOne/DayFour/LineTest/test/com/smoothstack/matthewcrowell/linetest/LineTestClass.java
@@ -2,13 +2,32 @@ package com.smoothstack.matthewcrowell.linetest;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class LineTestClass {
+	private Line firstLine = new Line(0, 0, 10, 10);
+	private Line secondLine = new Line(5, 5, 25, 25);
+	private Line thirdLine = new Line(-5, -5, 10, 25);
 
 	@Test
-	public void lineTest(){
-		Line line1 = new Line(1, 2, 4, 8);
-		assertEquals(2.0d, line1.getSlope(), .0001d);
+	public void lineGetSlopeTest(){
+
+		assertEquals(1.0d, firstLine.getSlope(), .0001d);
+		assertNotEquals(2.0d, firstLine.getSlope(), .0001d);
+	}
+
+	@Test
+	public void lineGetDistanceTest(){
+		Double distanceOfFirstLine = Math.sqrt((10 - 0) * (10 - 0) + (10 - 0) * (10 - 0));
+
+		assertEquals(distanceOfFirstLine, firstLine.getDistance(), .0001d);
+		assertNotEquals(Math.sqrt(2 * distanceOfFirstLine),
+				firstLine.getDistance(), .0001d);
+	}
+
+	@Test
+	public void lineParallelToTest(){
+		assertTrue(firstLine.parallelTo(secondLine));
+		assertFalse(firstLine.parallelTo(thirdLine));
 	}
 }
