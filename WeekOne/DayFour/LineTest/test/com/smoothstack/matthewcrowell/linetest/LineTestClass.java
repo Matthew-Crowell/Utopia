5ae0839dc80b3c904a9b2194f26efd102a1b4f78
Matthew-Crowell <Matthew.Crowell@Smoothstack.com>
2021-05-14 09:24:45 -0500

    Javadoc comments added for LineTestClass.

--- WeekOne/DayFour/LineTest/test/com/smoothstack/matthewcrowell/linetest/LineTestClass.java
+++ WeekOne/DayFour/LineTest/test/com/smoothstack/matthewcrowell/linetest/LineTestClass.java
@@ -4,20 +4,34 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+/**
+ * Class for unit testing of Line.class.
+ *
+ * @author matthew.crowell
+ */
 public class LineTestClass {
+	/* setup section shared by LineTestClass unit tests */
 	private Line firstLine = new Line(0, 0, 10, 10);
 	private Line secondLine = new Line(5, 5, 25, 25);
 	private Line thirdLine = new Line(-5, -5, 10, 25);
 
+	/**
+	 * Test for accurate positive and negative results
+	 * from Line.getSlope().
+	 */
 	@Test
-	public void lineGetSlopeTest(){
+	public void lineGetSlopeTest() {
 
 		assertEquals(1.0d, firstLine.getSlope(), .0001d);
 		assertNotEquals(2.0d, firstLine.getSlope(), .0001d);
 	}
 
+	/**
+	 * Test for accurate positive and negative results
+	 * from Line.getDistance().
+	 */
 	@Test
-	public void lineGetDistanceTest(){
+	public void lineGetDistanceTest() {
 		Double distanceOfFirstLine = Math.sqrt((10 - 0) * (10 - 0) + (10 - 0) * (10 - 0));
 
 		assertEquals(distanceOfFirstLine, firstLine.getDistance(), .0001d);
@@ -25,8 +39,12 @@ public class LineTestClass {
 				firstLine.getDistance(), .0001d);
 	}
 
+	/**
+	 * Test for accurate positive and negative results
+	 * from Line.parallelTo().
+	 */
 	@Test
-	public void lineParallelToTest(){
+	public void lineParallelToTest() {
 		assertTrue(firstLine.parallelTo(secondLine));
 		assertFalse(firstLine.parallelTo(thirdLine));
 	}
